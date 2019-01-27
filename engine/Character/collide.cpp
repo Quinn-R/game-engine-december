b6315e60afb6d2fac57c17e6811e82f31877d353
Quinn Reilly <quinn.reilly@hotmail.com>
2019-01-27 12:59:02 +0000

    update-27.01.19-1

--- engine/Character/collide.cpp
+++ engine/Character/collide.cpp
@@ -1,51 +1,30 @@
 #include "../Character.hpp"
 
-void Character::collide(/*int objToMove,*/ std::vector<sf::RectangleShape> &objects) {
-
-    //std::cout << "objects: " << objects.size() << std::endl;
+void Character::collide(std::vector<sf::RectangleShape> &objects) {
     for(int i = 0; i < objects.size(); i++) {
-        /*if(i == objToMove) {
-            //std::cout << "i: " << i << std::endl;
-            continue;
-        }*/
         
-        //std::cout << "objSize: " << i << " - " << objects[i].getSize().x << ", " << objects[i].getSize().y << std::endl;
-
-        if(sf::FloatRect(objectBumpers[0].getGlobalBounds()).intersects(sf::FloatRect(objects[i]/*.getShape()*/.getGlobalBounds()))/* && objects[i].objectIsCollidable == 1*/) {
-            //objectPos.x -= 1;
-            objectCollides[0] = 1;
-            /*objectCollides[1] = 0;
-            objectCollides[2] = 0;
-            objectCollides[3] = 0;*/
-        }/* else {
-            objectCollides[0] = 0;
-        }*/
-
-        if(sf::FloatRect(objectBumpers[1].getGlobalBounds()).intersects(sf::FloatRect(objects[i]/*.getShape()*/.getGlobalBounds()))/* && objects[i].objectIsCollidable == 1*/) {
-            /*objectCollides[0] = 0;*/
-            objectCollides[1] = 1;
-            /*objectCollides[2] = 0;
-            objectCollides[3] = 0;*/
-        }/* else {
-            objectCollides[1] = 0;
-        }*/
-
-        if(sf::FloatRect(objectBumpers[2].getGlobalBounds()).intersects(sf::FloatRect(objects[i]/*.getShape()*/.getGlobalBounds()))/* && objects[i].objectIsCollidable == 1*/) {
-            /*objectCollides[0] = 0;
-            objectCollides[1] = 0;*/
-            objectCollides[2] = 1;
-            /*objectCollides[3] = 0;*/
-        }/* else {
-            objectCollides[2] = 0;
-        }*/
-
-        if(sf::FloatRect(objectBumpers[3].getGlobalBounds()).intersects(sf::FloatRect(objects[i]/*.getShape()*/.getGlobalBounds()))/* && objects[i].objectIsCollidable == 1*/) {
-            /*objectCollides[0] = 0;
-            objectCollides[1] = 0;
-            objectCollides[2] = 0;*/
-            objectCollides[3] = 1;
-        }/* else {
-            objectCollides[3] = 0;
-        }*/
     }
 }
+
+
+/*
+void Character::collide(std::vector<sf::RectangleShape> &objects) {
+    for(int i = 0; i < objects.size(); i++) {
+        if(sf::FloatRect(objectBumpers[0].getGlobalBounds()).intersects(sf::FloatRect(objects[i].getShape().getGlobalBounds()))) {
+            objectCollides[0] = 1;
+        }
+
+        if(sf::FloatRect(objectBumpers[1].getGlobalBounds()).intersects(sf::FloatRect(objects[i].getShape().getGlobalBounds()))) {
+            objectCollides[1] = 1;
+        }
+
+        if(sf::FloatRect(objectBumpers[2].getGlobalBounds()).intersects(sf::FloatRect(objects[i].getShape().getGlobalBounds()))) {
+            objectCollides[2] = 1;
+        }
+
+        if(sf::FloatRect(objectBumpers[3].getGlobalBounds()).intersects(sf::FloatRect(objects[i].getShape().getGlobalBounds()))) {
+            objectCollides[3] = 1;
+        }
+    }
+}
+*/
--- engine/Character/move.cpp
+++ engine/Character/move.cpp
@@ -1,69 +1,35 @@
 #include "../Character.hpp"
 
-void Character::move(std::vector<int> &moves, float dtAsSeconds/*, int objToMove*/, std::vector<sf::RectangleShape> &objects) {
-    collide(/*objToMove, */objects);
+void Character::move(std::vector<int> &moves, float dtAsSeconds, std::vector<sf::RectangleShape> &objects) {
+    collide(objects);
     setBumperPosition();
 
     sf::Vector2f objectPos = getPosition();
 
     if(moves[0] == 1 && objectCollides[0] == 0) {
         objectPos.x -= objectSpeed * dtAsSeconds;
-    }/* else {
-        objectCollides[0] = 0;
-        objectCollides[1] = 0;
-        objectCollides[2] = 0;
-        objectCollides[3] = 0;
-    } */
+    }
 
     if(moves[1] == 1 && objectCollides[1] == 0) {
         objectPos.x += objectSpeed * dtAsSeconds;
-    }/* else {
-        objectCollides[0] = 0;
-        objectCollides[1] = 0;
-        objectCollides[2] = 0;
-        objectCollides[3] = 0;
-    } */
+    }
 
     if(moves[2] == 1 && objectCollides[2] == 0) {
         objectPos.y -= objectSpeed * dtAsSeconds;
-    }/* else {
-        objectCollides[0] = 0;
-        objectCollides[1] = 0;
-        objectCollides[2] = 0;
-        objectCollides[3] = 0;
-    } */
+    }
 
     if(moves[3] == 1 && objectCollides[3] == 0) {
         objectPos.y += objectSpeed * dtAsSeconds;
-    }/* else {
-        objectCollides[0] = 0;
-        objectCollides[1] = 0;
-        objectCollides[2] = 0;
-        objectCollides[3] = 0;
-    } */
+    }
     
-    /*std::cout << "moves: " << moves[0] << moves[1] << moves[2] << moves[3] << std::endl;
-    std::cout << "pos: " << getPosition().x <<  getPosition().y << std::endl;
-    std::cout << "objPos: " << objectPos.x <<  objectPos.y << std::endl;
-    std::cout << "time: " << dtAsSeconds << std::endl;*/
     std::cout << "collide: " << objectCollides[0] << objectCollides[1] << objectCollides[2] << objectCollides[3] << std::endl;
     
-    /*objectShape.*/setPosition(objectPos);
+    
+    objectCollides[0] = 0;
+    objectCollides[1] = 0;
+    objectCollides[2] = 0;
+    objectCollides[3] = 0;
+    
+    setPosition(objectPos);
     setBumperPosition();
 }
-
-/*
-void Object::update(float elapsedTime)
-{
-	if(isMovingLeft)
-    {
-        objPos.x -= speed * elapsedTime;
-    }
-    if(isMovingRight)
-    {
-        objPos.x += speed * elapsedTime;
-    }
-
-    objShape.setPosition(objPos);
-}
-*/
