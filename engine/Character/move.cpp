0eb8574758c615940f44a837b24b21c74b5785d7
Quinn Reilly <quinn.reilly@hotmail.com>
2019-02-17 15:01:08 +0000

    update-17.02.19-1

--- engine/Character/move.cpp
+++ engine/Character/move.cpp
@@ -1,39 +1,43 @@
 #include "../Character.hpp"
 
 void Character::move(std::vector<int> &moves, float dtAsSeconds, std::vector<sf::RectangleShape> &objects, int objCollider) {
-    collide(objects, objCollider);
-
+    
+    
     sf::Vector2f objectPos = getPosition();
 
     if(moves[0] == 1 && objectCollides[0] == 0) {
         objectPos.x -= objectSpeed * dtAsSeconds;
     } else if(objectCollides[0] == 1) {
-        objectPos.x += intersect.x;
+        objectPos.x -= intersect.x;
+        // objectPos.x += 1;
     }
 
     if(moves[1] == 1 && objectCollides[1] == 0) {
         objectPos.x += objectSpeed * dtAsSeconds;
     } else if(objectCollides[1] == 1) {
-        objectPos.x -= intersect.x;
+        objectPos.x += intersect.x;
+        // objectPos.x -= 1;
     }
 
     if(moves[2] == 1 && objectCollides[2] == 0) {
         objectPos.y -= objectSpeed * dtAsSeconds;
     } else if(objectCollides[2] == 1) {
-        objectPos.y += intersect.y;
+        objectPos.y -= intersect.y;
+        // objectPos.y += 1;
     }
 
     if(moves[3] == 1 && objectCollides[3] == 0) {
         objectPos.y += objectSpeed * dtAsSeconds;
     } else if(objectCollides[3] == 1) {
-        std::cout << "intersect.y = " << intersect.y << std::endl;
-        objectPos.y -= intersect.y;
-        std::cout << "objectPos.y = " << objectPos.y << std::endl;
+        objectPos.y += intersect.y;
+        // objectPos.y -= 1;
     }
-    
+    //std::cout << "intersect.y = " << intersect.y << std::endl;
+    //std::cout << "objectPos.y = " << objectPos.y << std::endl;
     //std::cout << "collide: " << objectCollides[0] << objectCollides[1] << objectCollides[2] << objectCollides[3] << std::endl;
     
     resetCollide();
     setPosition(objectPos);
+    collide(objects, objCollider);
     //setBumperPosition();
 }
--- engine/SfmlEngine/SfmlEngine.cpp
+++ engine/SfmlEngine/SfmlEngine.cpp
@@ -5,7 +5,7 @@ SfmlEngine::SfmlEngine() {
 	windowRes.y = sf::VideoMode::getDesktopMode().height;
 	
 	window.create(sf::VideoMode(windowRes.x, windowRes.y), "", sf::Style::Fullscreen);
-	view.reset(sf::FloatRect(-16.f, -16.f, (windowRes.x - 16) / 6, (windowRes.y - 16) / 6));
+	view.reset(sf::FloatRect(-100, -100, (windowRes.x - 16) /*/ 6*/, (windowRes.y - 16) /*/ 6*/));
 	window.setView(view);
 	
 	for(int i = 0; i < 4; i++) {
